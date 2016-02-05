66a8bf90150b0e5788b06d4f299a56cac56cab32
kimx <gn01124620@gmail.com>
2016-02-05 10:08:39 +0800

    1

---
+++ FormAuthecicateionUserDataLab/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using FormAuthecicateionUserDataLab.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuthecicateionUserDataLab.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ViewResult SignIn()
+        {
+            return View("SignIn");
+        }
+
+
+        [HttpPost]
+        public ActionResult SignIn(SignInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("SignIn");
+            }
+
+            DateTime cookieIssuedDate = DateTime.Now;
+
+            var ticket = new FormsAuthenticationTicket(0,
+                model.UserName,
+                cookieIssuedDate,
+                cookieIssuedDate.AddMinutes(FormsAuthentication.Timeout.TotalMinutes),
+                false,
+                model.CustomUserData,
+                FormsAuthentication.FormsCookiePath);
+
+            string encryptedCookieContent = FormsAuthentication.Encrypt(ticket);
+
+            var formsAuthenticationTicketCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedCookieContent)
+            {
+                Domain = FormsAuthentication.CookieDomain,
+                Path = FormsAuthentication.FormsCookiePath,
+                HttpOnly = true,
+                Secure = FormsAuthentication.RequireSSL
+            };
+
+            System.Web.HttpContext.Current.Response.Cookies.Add(formsAuthenticationTicketCookie);
+
+            return RedirectToAction("Home");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ViewResult Home()
+        {
+            return View("Home");
+        }
+    }
+}
---
+++ FormAuthecicateionUserDataLab/Global.asax.cs
@@ -0,0 +1,62 @@
+﻿using FormAuthecicateionUserDataLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace FormAuthecicateionUserDataLab
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Account", action = "Home", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_OnPostAuthenticateRequest(object sender, EventArgs e)
+        {
+            // Get a reference to the current User
+            IPrincipal currentUser = HttpContext.Current.User;
+
+            // If we are dealing with an authenticated forms authentication request
+            if (currentUser.Identity.IsAuthenticated
+                && currentUser.Identity.GetType() != typeof(CustomIdentity))
+            {
+                // Create a CustomIdentity based on the FormsAuthenticationTicket           
+                var identity = new CustomIdentity(((FormsIdentity)currentUser.Identity).Ticket);
+
+                // Create the CustomPrincipal
+                var user = new GenericPrincipal(identity, null);
+
+                // Attach the CustomPrincipal to HttpContext.User and Thread.CurrentPrincipal
+                HttpContext.Current.User = user;
+                Thread.CurrentPrincipal = user;
+            }
+        }
+    }
+}
